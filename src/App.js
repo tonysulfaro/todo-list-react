a943ece1526fb937a71b398fe22d02430a5df120
Tony Sulfaro <tony116523@gmail.com>
2020-02-27 00:00:29 -0500

    dumping of todos implemented

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Todo from './Todo';
 import TodoInput from './TodoInput';
 
-let todos = [{task:"get groceries", description: "go to kroger to get a gallon of milk"}]
+let todos = [{task:"get groceries", description: "go to kroger to get a gallon of milk"},
+{task:"get groceries", description: "go to kroger to get a gallon of milk"}]
 
 function App() {
   return (
     <div className="container">
       <h1>TODO List</h1>
       <TodoInput></TodoInput>
-      <Todo todo={todos[0]}></Todo>
+
+      {/* dump out todos save in list */}
+      {todos.map((todo, index) => {
+        console.log(todo)
+        return (
+          <Todo key={index.toString()} todo={todo}></Todo>
+        )
+      })}
     </div>
   );
 }
